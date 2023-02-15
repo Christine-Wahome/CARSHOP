292d5bff5e2bd950d25933c6a0291d91d69ad336
Christine <wanjiruwahome232@gmail.com>
2023-02-15 20:36:34 +0300

    Cart and Email Backend

--- Backend/src/Router/cartRouter.ts
+++ Backend/src/Router/cartRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";   
-import {addProductsToCart} from '../Controller/cartController'
+import {addProductsToCart, subtractProducts, addProducts} from '../Controller/cartController'
 import { verifyToken } from "../Middlewares/verifyToken";
 
 const cartRoute = Router()
 
-cartRoute.post("",verifyToken,addProductsToCart)
+cartRoute.post("",verifyToken, addProductsToCart)
+cartRoute.post("/subtract/:cardID",verifyToken, subtractProducts)
+cartRoute.post("/add/:cardID",verifyToken, addProducts)
 
 export default cartRoute
